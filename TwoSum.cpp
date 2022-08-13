f6b05ce642be2586266292bdde588badb0e13752
Ankush raj <104245302+AnkushTech2022@users.noreply.github.com>
2022-08-13 22:48:52 +0530

    Update TwoSum.cpp

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,4 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+  int arr[100];
+  int sum;
+  cin>>sum;
+  int size;
+  cin>>size;
+  for(int i=0;i<size;i++){
+  for(int j=i+1;j<size;j++){
+  if(arr[i]+arr[j]==sum){
+  cout<<i<<" "<<j<<endl; 
+  }
+  }
+  }
+  return 0;
 }
