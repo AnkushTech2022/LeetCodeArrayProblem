bfdb923790d3deafcb40a549093418a1954db19d
Ankush raj <104245302+AnkushTech2022@users.noreply.github.com>
2022-08-20 20:40:22 +0530

    Update TwoSum.cpp

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -15,3 +15,28 @@ int main(){
   }
   return 0;
 }
+
+
+
+//java code of two sum:
+import java.util.*;
+class twoSum{
+public static int twoSum[](int arr[], int target){
+  for(int i=0;i<arr.length;i++){
+  for(int j=i+1;j<arr.length;j++){
+  if(arr[i]+arr[j]==target){
+  return new int[]{i,j};
+  }}}
+  
+  return null;
+}
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int size=sc.nextInt();
+int arrr[]=new int[1000];
+for(int i=0;i<size;i++){
+arrr[i]=sc.nextInt();
+}
+  int target=sc.nextInt();
+  
+twoSum(arr, target);}}
